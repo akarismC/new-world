f0731a0c4b26ca5625561c00129c8b3ad38dc7c9
Jingyi Cao <654056790@qq.com>
2018-06-10 23:16:38 +0800

    change to word-based comparison

--- src/main/java/com/ea/newworld/issue/FeatureString.java
+++ src/main/java/com/ea/newworld/issue/FeatureString.java
@@ -2,69 +2,93 @@ package com.ea.newworld.issue;
 
 import com.ea.newworld.utils.Hirschberg;
 
+import java.util.*;
+
 public class FeatureString {
     private String features;
+    private List<String> featureList;
+    private HashMap<String, String> classMap = new HashMap<String, String>();
 
     public FeatureString(String string){
-        features = preprocess(string);
+        featureList = preprocess(string);
+        //features = preprocess(string);
     }
 
     public void updateWithFile(String input){
         System.out.println(" *** start to update feature string with new log *** ");
 
-        input = preprocess(input);
-//        int m = features.length();
-//        int n = input.length();
-//
-//        int L[][] = new int[m+1][n+1];
-//
-//        for (int i=0; i<=m; i++)
-//        {
-//            for (int j=0; j<=n; j++)
-//            {
-//                if (i == 0 || j == 0)
-//                    L[i][j] = 0;
-//                else if (features.charAt(i-1) == input.charAt(j-1))
-//                    L[i][j] = L[i-1][j-1] + 1;
-//                else
-//                    L[i][j] = L[i-1][j] >= L[i][j-1] ? L[i-1][j] : L[i][j-1];
-//            }
-//        }
-//
-//        String result = "";
-//        int start = n;
-//
-//        for(int i = m; i > 0; i--){
-//            for(int j = start; j > 0; j--){
-//                if(L[i][j] == L[i-1][j]){
-//                    break;
-//                }else if( L[i][j] == L[i][j-1]){
-//                    continue;
-//                }
-//                else if(L[i][j] == (L[i-1][j-1] + 1)){
-//                    result = input.charAt(j-1) + result;
-//                    start = j-1;
-//                    break;
-//                }
-//            }
-//        }
-//
-//        features = result;
-        Hirschberg hirschberg = new Hirschberg();
-        features = hirschberg.algC(features.length(), input.length(), features, input);
+        List<String> list = preprocess(input);
+        int m = featureList.size();
+        int n = list.size();
+
+        int L[][] = new int[m+1][n+1];
+
+        for (int i=0; i<=m; i++)
+        {
+            for (int j=0; j<=n; j++)
+            {
+                if (i == 0 || j == 0)
+                    L[i][j] = 0;
+                else if (featureList.get(i-1).equalsIgnoreCase(list.get(j-1)))
+                    L[i][j] = L[i-1][j-1] + 1;
+                else
+                    L[i][j] = L[i-1][j] >= L[i][j-1] ? L[i-1][j] : L[i][j-1];
+            }
+        }
+
+        List<String> result = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder();
+        int start = n;
+
+        for(int i = m; i > 0; i--){
+            for(int j = start; j > 0; j--){
+                if(L[i][j] == L[i-1][j]){
+                    break;
+                }else if( L[i][j] == L[i][j-1]){
+                    continue;
+                }
+                else if(L[i][j] == (L[i-1][j-1] + 1)){
+                    result.add(list.get(j-1));
+                    sb.append(list.get(j-1));
+                    start = j-1;
+                    break;
+                }
+            }
+        }
+
+        featureList = result;
+        features = sb.toString();
+//        Hirschberg hirschberg = new Hirschberg();
+//        features = hirschberg.algC(features.length(), input.length(), features, input);
 
         System.out.println(" *** end updating feature string with new log *** ");
     }
 
-    private String preprocess(String origin){
+    private List<String> preprocess(String origin){
         String result = origin.replaceAll("\\d\\d\\/\\d\\d\\/\\d\\d \\d\\d:\\d\\d:\\d\\d:\\d\\d\\d: ", "");
-        result = result.replaceAll("\\[SEQID:\\d+\\] \\[REQID:Integration-Tests\\] \\[ATAG:.+\\]: INFO ", "");
+        result = result.replaceAll("\\[SEQID:\\d+\\] \\[REQID:Integration-Tests\\] \\[ATAG:.+\\]: INFO  ", "");
+        result = result.replaceAll("-|\\(|\\)|:", " ");
+        result = result.replaceAll(" +", " ");
+        //result = result.replaceAll("\\r?\\n", "");
+        features = result;
+        return replaceClassNames(result);
+    }
+
+    private List<String> replaceClassNames(String s){
+//        String[] strings = s.split("\\r?\\n");
+//        List<String> result = new ArrayList<>();
+//        for(String line : strings){
+//            result.addAll(Arrays.asList(line.split(" ")));
+//        }
+        List<String> result = Arrays.asList(s.split(" "));
         return result;
     }
 
     public String getFeatures(){
         return features;
     }
+
+    public List<String> getList() { return  featureList;}
 
     @Override
     public String toString() {
--- src/main/java/com/ea/newworld/issue/Issue.java
+++ src/main/java/com/ea/newworld/issue/Issue.java
@@ -1,5 +1,7 @@
 package com.ea.newworld.issue;
 
+import java.util.List;
+
 public class Issue {
     private String issueName;
     private FeatureString featureString;
@@ -18,18 +20,32 @@ public class Issue {
     }
 
     public boolean match(String log){
-        String feature = featureString.getFeatures();
-        int length = feature.length();
+//        String feature = featureString.getFeatures();
+//        int length = feature.length();
+//        int index = 0;
+//        for(int i = 0; i < length; i++){
+//            index = log.indexOf(feature.charAt(i), index);
+//            if(index == -1){
+//                System.out.println("Strings unable to match:" + feature.substring(i));
+//                return false;
+//            }
+//            index++;
+//        }
+
+        List<String> list = featureString.getList();
         int index = 0;
-        for(int i = 0; i < length; i++){
-            index = log.indexOf(feature.charAt(i), index);
+        for(int i = 0; i < list.size(); i++){
+            index = log.indexOf(list.get(i), index);
             if(index == -1){
-                System.out.println("Strings unable to match:" + feature.substring(i));
+                System.out.println("Strings unable to match:");
+                for(int j = i; j < list.size(); j++){
+                    System.out.print(list.get(j));
+                }
+                System.out.println();
                 return false;
             }
-            index++;
+            index = index + list.get(i).length();
         }
-
         return true;
     }
 }
